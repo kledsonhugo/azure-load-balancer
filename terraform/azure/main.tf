e6b0392d7519ad2015d2e4fb4fc3d1922b1fba2a
Kledson Basso <kledsonhugo@gmail.com>
2024-09-13 16:48:57 -0300

    Update provider and workflow parameters

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,179 +3,179 @@ resource "azurerm_resource_group" "rg" {
     location = "brazilsouth"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-    name                = "vnet"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    address_space       = ["10.0.0.0/16"]
-}
+# resource "azurerm_virtual_network" "vnet" {
+#     name                = "vnet"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     address_space       = ["10.0.0.0/16"]
+# }
 
-resource "azurerm_subnet" "subnet" {
-    name                 = "subnet"
-    resource_group_name  = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes     = ["10.0.1.0/24"]
-}
+# resource "azurerm_subnet" "subnet" {
+#     name                 = "subnet"
+#     resource_group_name  = azurerm_resource_group.rg.name
+#     virtual_network_name = azurerm_virtual_network.vnet.name
+#     address_prefixes     = ["10.0.1.0/24"]
+# }
 
-resource "azurerm_network_security_group" "nsg" {
-    name                = "nsg"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    security_rule {
-        name                       = "HTTP"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-}
+# resource "azurerm_network_security_group" "nsg" {
+#     name                = "nsg"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     security_rule {
+#         name                       = "HTTP"
+#         priority                   = 1001
+#         direction                  = "Inbound"
+#         access                     = "Allow"
+#         protocol                   = "Tcp"
+#         source_port_range          = "*"
+#         destination_port_range     = "80"
+#         source_address_prefix      = "*"
+#         destination_address_prefix = "*"
+#     }
+# }
 
-resource "azurerm_subnet_network_security_group_association" "nsg" {
-    subnet_id                 = azurerm_subnet.subnet.id
-    network_security_group_id = azurerm_network_security_group.nsg.id
-}
+# resource "azurerm_subnet_network_security_group_association" "nsg" {
+#     subnet_id                 = azurerm_subnet.subnet.id
+#     network_security_group_id = azurerm_network_security_group.nsg.id
+# }
 
-resource "azurerm_network_interface" "vm01" {
-    name                = "vm01"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    ip_configuration {
-        name                          = "vm01"
-        subnet_id                     = azurerm_subnet.subnet.id
-        private_ip_address_allocation = "Dynamic"
-    }
-}
+# resource "azurerm_network_interface" "vm01" {
+#     name                = "vm01"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     ip_configuration {
+#         name                          = "vm01"
+#         subnet_id                     = azurerm_subnet.subnet.id
+#         private_ip_address_allocation = "Dynamic"
+#     }
+# }
 
-resource "azurerm_network_interface" "vm02" {
-    name                = "vm02"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    ip_configuration {
-        name                          = "vm02"
-        subnet_id                     = azurerm_subnet.subnet.id
-        private_ip_address_allocation = "Dynamic"
-    }
-}
+# resource "azurerm_network_interface" "vm02" {
+#     name                = "vm02"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     ip_configuration {
+#         name                          = "vm02"
+#         subnet_id                     = azurerm_subnet.subnet.id
+#         private_ip_address_allocation = "Dynamic"
+#     }
+# }
 
-data "template_file" "cloud_init" {
-    template = "${file("./scripts/cloud_init.sh")}"
-}
+# data "template_file" "cloud_init" {
+#     template = "${file("./scripts/cloud_init.sh")}"
+# }
 
-resource "azurerm_availability_set" "vm" {
-    name                = "vm"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_availability_set" "vm" {
+#     name                = "vm"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_virtual_machine" "vm01" {
-    name                             = "vm01"
-    location                         = azurerm_resource_group.rg.location
-    resource_group_name              = azurerm_resource_group.rg.name
-    network_interface_ids            = [azurerm_network_interface.vm01.id]
-    availability_set_id              = azurerm_availability_set.vm.id
-    vm_size                          = "Standard_DS1_v2"
-    delete_os_disk_on_termination    = true
-    delete_data_disks_on_termination = true
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "0001-com-ubuntu-server-jammy"
-        sku       = "22_04-lts"
-        version   = "latest"
-    }
-    storage_os_disk {
-        name              = "vm01"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
-    os_profile {
-        computer_name  = "vm01"
-        admin_username = "vmuser"
-        admin_password = "Password1234!"
-        custom_data    = "${base64encode(data.template_file.cloud_init.rendered)}"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-}
+# resource "azurerm_virtual_machine" "vm01" {
+#     name                             = "vm01"
+#     location                         = azurerm_resource_group.rg.location
+#     resource_group_name              = azurerm_resource_group.rg.name
+#     network_interface_ids            = [azurerm_network_interface.vm01.id]
+#     availability_set_id              = azurerm_availability_set.vm.id
+#     vm_size                          = "Standard_DS1_v2"
+#     delete_os_disk_on_termination    = true
+#     delete_data_disks_on_termination = true
+#     storage_image_reference {
+#         publisher = "Canonical"
+#         offer     = "0001-com-ubuntu-server-jammy"
+#         sku       = "22_04-lts"
+#         version   = "latest"
+#     }
+#     storage_os_disk {
+#         name              = "vm01"
+#         caching           = "ReadWrite"
+#         create_option     = "FromImage"
+#         managed_disk_type = "Standard_LRS"
+#     }
+#     os_profile {
+#         computer_name  = "vm01"
+#         admin_username = "vmuser"
+#         admin_password = "Password1234!"
+#         custom_data    = "${base64encode(data.template_file.cloud_init.rendered)}"
+#     }
+#     os_profile_linux_config {
+#         disable_password_authentication = false
+#     }
+# }
 
-resource "azurerm_virtual_machine" "vm02" {
-    name                             = "vm02"
-    location                         = azurerm_resource_group.rg.location
-    resource_group_name              = azurerm_resource_group.rg.name
-    network_interface_ids            = [azurerm_network_interface.vm02.id]
-    availability_set_id              = azurerm_availability_set.vm.id
-    vm_size                          = "Standard_DS1_v2"
-    delete_os_disk_on_termination    = true
-    delete_data_disks_on_termination = true
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "0001-com-ubuntu-server-jammy"
-        sku       = "22_04-lts"
-        version   = "latest"
-    }
-    storage_os_disk {
-        name              = "vm02"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
-    os_profile {
-        computer_name  = "vm02"
-        admin_username = "vmuser"
-        admin_password = "Password1234!"
-        custom_data    = "${base64encode(data.template_file.cloud_init.rendered)}"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-}
+# resource "azurerm_virtual_machine" "vm02" {
+#     name                             = "vm02"
+#     location                         = azurerm_resource_group.rg.location
+#     resource_group_name              = azurerm_resource_group.rg.name
+#     network_interface_ids            = [azurerm_network_interface.vm02.id]
+#     availability_set_id              = azurerm_availability_set.vm.id
+#     vm_size                          = "Standard_DS1_v2"
+#     delete_os_disk_on_termination    = true
+#     delete_data_disks_on_termination = true
+#     storage_image_reference {
+#         publisher = "Canonical"
+#         offer     = "0001-com-ubuntu-server-jammy"
+#         sku       = "22_04-lts"
+#         version   = "latest"
+#     }
+#     storage_os_disk {
+#         name              = "vm02"
+#         caching           = "ReadWrite"
+#         create_option     = "FromImage"
+#         managed_disk_type = "Standard_LRS"
+#     }
+#     os_profile {
+#         computer_name  = "vm02"
+#         admin_username = "vmuser"
+#         admin_password = "Password1234!"
+#         custom_data    = "${base64encode(data.template_file.cloud_init.rendered)}"
+#     }
+#     os_profile_linux_config {
+#         disable_password_authentication = false
+#     }
+# }
 
-resource "azurerm_public_ip" "lb" {
-    name                = "lb"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method   = "Dynamic"
-    domain_name_label   = "staticsite-lb-bassokl"
-}
+# resource "azurerm_public_ip" "lb" {
+#     name                = "lb"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     allocation_method   = "Dynamic"
+#     domain_name_label   = "staticsite-lb-bassokl"
+# }
 
-resource "azurerm_lb" "lb" {
-    name                = "lb"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    frontend_ip_configuration {
-        name                 = "lb"
-        public_ip_address_id = azurerm_public_ip.lb.id
-    }
-}
+# resource "azurerm_lb" "lb" {
+#     name                = "lb"
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     frontend_ip_configuration {
+#         name                 = "lb"
+#         public_ip_address_id = azurerm_public_ip.lb.id
+#     }
+# }
 
-resource "azurerm_lb_backend_address_pool" "lb" {
-    name            = "lb"
-    loadbalancer_id = azurerm_lb.lb.id
-}
+# resource "azurerm_lb_backend_address_pool" "lb" {
+#     name            = "lb"
+#     loadbalancer_id = azurerm_lb.lb.id
+# }
 
-resource "azurerm_lb_rule" "lb" {
-    name                           = "HTTP"
-    loadbalancer_id                = azurerm_lb.lb.id
-    protocol                       = "Tcp"
-    frontend_port                  = 80
-    backend_port                   = 80
-    frontend_ip_configuration_name = "lb"
-    backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb.id]
-}
+# resource "azurerm_lb_rule" "lb" {
+#     name                           = "HTTP"
+#     loadbalancer_id                = azurerm_lb.lb.id
+#     protocol                       = "Tcp"
+#     frontend_port                  = 80
+#     backend_port                   = 80
+#     frontend_ip_configuration_name = "lb"
+#     backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb.id]
+# }
 
-resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
-    ip_configuration_name   = "vm01"
-    network_interface_id    = azurerm_network_interface.vm01.id
-    backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
-}
+# resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
+#     ip_configuration_name   = "vm01"
+#     network_interface_id    = azurerm_network_interface.vm01.id
+#     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
+# }
 
-resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
-    ip_configuration_name   = "vm02"
-    network_interface_id    = azurerm_network_interface.vm02.id
-    backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
-}
+# resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
+#     ip_configuration_name   = "vm02"
+#     network_interface_id    = azurerm_network_interface.vm02.id
+#     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
+# }
