86da3a3b99f62d75e05ac0014b6c8058d8ea65d0
Kledson Basso <kledsonhugo@gmail.com>
2024-10-02 15:55:56 -0300

    Atualiza o nome do grupo de recursos no provider.tf para simplificar a configuração

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-lb-tf-backend"
+    resource_group_name  = "rg-staticsite-lb-tf"
     storage_account_name = "staticsitelbtf"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
