2b0f8cefe698ba9d2128849aef70aca018e3e525
Kledson Basso <kledsonhugo@gmail.com>
2024-11-21 08:57:12 -0300

    Atualiza nomes do grupo de recursos e da conta de armazenamento no provider.tf

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-lb-tf-aluno02"
-    storage_account_name = "staticsitelbtfaluno02"
+    resource_group_name  = "rg-staticsite-lb-tf"
+    storage_account_name = "staticsitelbtf2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -23,6 +23,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-staticsite-lb-aluno02"
+    name     = "rg-staticsite-lb"
     location = "brazilsouth"
 }
