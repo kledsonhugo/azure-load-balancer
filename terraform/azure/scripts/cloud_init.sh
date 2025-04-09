627038d76f3f81ca1d92d72724aca73c70c850a0
Kledson Basso <kledsonhugo@gmail.com>
2025-04-09 11:46:01 -0300

    Update Terraform plan command to include destroy option and modify cloud_init.sh to output HTML for application info

--- terraform/azure/scripts/cloud_init.sh
+++ terraform/azure/scripts/cloud_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
 sudo apt install apache2 -y
-echo "More info: https://github.com/kledsonhugo/app-staticsite-lb-azure" > /var/www/html/index.html
+echo '<html><body>More info: <a href="https://github.com/kledsonhugo/app-staticsite-lb-azure"></a></body></html>' > /var/www/html/index.html
