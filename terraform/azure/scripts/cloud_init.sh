0f55330a091d26b3604b8770bb5f22f41e3af3a4
Kledson Basso <kledsonhugo@gmail.com>
2025-04-09 12:07:50 -0300

    Update cloud_init.sh to change link text for GitHub repository in HTML output

--- terraform/azure/scripts/cloud_init.sh
+++ terraform/azure/scripts/cloud_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
 sudo apt install apache2 -y
-echo '<html><body><a href=https://github.com/kledsonhugo/app-staticsite-lb-azure>More info</a></body></html>' > /var/www/html/index.html
+echo '<html><body><a href=https://github.com/kledsonhugo/app-staticsite-lb-azure>app-staticsite-lb-azure</a></body></html>' > /var/www/html/index.html
