a00c8a6de2526817964222c27ac5f2693c0cdd88
Kledson Basso <kledsonhugo@gmail.com>
2025-04-09 10:54:30 -0300

    Add output for Load Balancer FQDN and update cloud initialization script for clarity

--- terraform/azure/scripts/cloud_init.sh
+++ terraform/azure/scripts/cloud_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
 sudo apt install apache2 -y
-echo "More info: <a href=https://github.com/kledsonhugo/app-staticsite-lb-azure>GitHub Repository</a>" > /var/www/html/index.html
+echo "More info: https://github.com/kledsonhugo/app-staticsite-lb-azure" > /var/www/html/index.html
