e4c0d90660426ab6c2f4dfb9bf976b8322c4bcdb
Kledson Basso <kledsonhugo@gmail.com>
2025-04-25 11:27:50 -0300

    Remove destroy option from Terraform Plan command in workflow.yaml

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -65,16 +65,16 @@ resource "azurerm_network_interface" "vm01-nic" {
   }
 }
 
-resource "azurerm_network_interface" "vm02-nic" {
-  name                = "vm02-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "vm02"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+# resource "azurerm_network_interface" "vm02-nic" {
+#   name                = "vm02-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   ip_configuration {
+#     name                          = "vm02"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
 data "template_file" "cloud_init" {
   template = file("./scripts/cloud_init.sh")
@@ -118,81 +118,81 @@ resource "azurerm_virtual_machine" "vm01" {
   }
 }
 
-resource "azurerm_virtual_machine" "vm02" {
-  name                             = "vm02"
-  location                         = azurerm_resource_group.rg.location
-  resource_group_name              = azurerm_resource_group.rg.name
-  network_interface_ids            = [azurerm_network_interface.vm02-nic.id]
-  availability_set_id              = azurerm_availability_set.vm.id
-  vm_size                          = "Standard_D2s_v3"
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "vm02-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "vm02"
-    admin_username = "vmuser"
-    admin_password = "Password1234!"
-    custom_data    = base64encode(data.template_file.cloud_init.rendered)
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
+# resource "azurerm_virtual_machine" "vm02" {
+#   name                             = "vm02"
+#   location                         = azurerm_resource_group.rg.location
+#   resource_group_name              = azurerm_resource_group.rg.name
+#   network_interface_ids            = [azurerm_network_interface.vm02-nic.id]
+#   availability_set_id              = azurerm_availability_set.vm.id
+#   vm_size                          = "Standard_D2s_v3"
+#   delete_os_disk_on_termination    = true
+#   delete_data_disks_on_termination = true
+#   storage_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+#   storage_os_disk {
+#     name              = "vm02-os-disk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "vm02"
+#     admin_username = "vmuser"
+#     admin_password = "Password1234!"
+#     custom_data    = base64encode(data.template_file.cloud_init.rendered)
+#   }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+# }
 
-resource "azurerm_public_ip" "lb" {
-  name                = "lb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  domain_name_label   = "staticsitelbtf0001"
-  sku                 = "Standard"
-}
+# resource "azurerm_public_ip" "lb" {
+#   name                = "lb"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   allocation_method   = "Static"
+#   domain_name_label   = "staticsitelbtf0001"
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_lb" "lb" {
-  name                = "lb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-  frontend_ip_configuration {
-    name                 = "lb"
-    public_ip_address_id = azurerm_public_ip.lb.id
-  }
-}
+# resource "azurerm_lb" "lb" {
+#   name                = "lb"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   sku                 = "Standard"
+#   frontend_ip_configuration {
+#     name                 = "lb"
+#     public_ip_address_id = azurerm_public_ip.lb.id
+#   }
+# }
 
-resource "azurerm_lb_backend_address_pool" "lb" {
-  name            = "lb"
-  loadbalancer_id = azurerm_lb.lb.id
-}
+# resource "azurerm_lb_backend_address_pool" "lb" {
+#   name            = "lb"
+#   loadbalancer_id = azurerm_lb.lb.id
+# }
 
-resource "azurerm_lb_rule" "lb" {
-  name                           = "HTTP"
-  loadbalancer_id                = azurerm_lb.lb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "lb"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb.id]
-}
+# resource "azurerm_lb_rule" "lb" {
+#   name                           = "HTTP"
+#   loadbalancer_id                = azurerm_lb.lb.id
+#   protocol                       = "Tcp"
+#   frontend_port                  = 80
+#   backend_port                   = 80
+#   frontend_ip_configuration_name = "lb"
+#   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb.id]
+# }
 
-resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
-  ip_configuration_name   = "vm01"
-  network_interface_id    = azurerm_network_interface.vm01-nic.id
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
-}
+# resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
+#   ip_configuration_name   = "vm01"
+#   network_interface_id    = azurerm_network_interface.vm01-nic.id
+#   backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
+# }
 
-resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
-  ip_configuration_name   = "vm02"
-  network_interface_id    = azurerm_network_interface.vm02-nic.id
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
-}
+# resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
+#   ip_configuration_name   = "vm02"
+#   network_interface_id    = azurerm_network_interface.vm02-nic.id
+#   backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
+# }
