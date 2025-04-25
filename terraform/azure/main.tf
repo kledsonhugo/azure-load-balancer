668e98df8113fb6fa1f6b49e165e67b6815326c3
Kledson Basso <kledsonhugo@gmail.com>
2025-04-25 12:12:22 -0300

    Update vm02 public IP allocation method to Static

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -69,7 +69,7 @@ resource "azurerm_public_ip" "vm02" {
   name                = "vm02-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
