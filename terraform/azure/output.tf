1d70946f26bc872db4a1c144f095b864c22036e3
Kledson Basso <kledsonhugo@gmail.com>
2025-04-25 11:30:19 -0300

    Comment out Load Balancer FQDN output in Terraform configuration

--- terraform/azure/output.tf
+++ terraform/azure/output.tf
@@ -1,4 +1,4 @@
-output "lb_fqdn" {
-  value       = "http://${azurerm_public_ip.lb.fqdn}"
-  description = "FQDN público do Load Balancer"
-}
+# output "lb_fqdn" {
+#   value       = "http://${azurerm_public_ip.lb.fqdn}"
+#   description = "FQDN público do Load Balancer"
+# }
