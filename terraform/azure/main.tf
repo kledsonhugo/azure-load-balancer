d6502cdd0589a07e6fabf63c4736d3d691c278fd
Kledson Basso <kledsonhugo@gmail.com>
2025-04-25 11:07:37 -0300

    Remove destroy option from Terraform Plan command and add static public IP resource for VM01

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -45,6 +45,14 @@ resource "azurerm_subnet_network_security_group_association" "nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+resource "azurerm_public_ip" "vm01" {
+  name                = "vm01-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
 resource "azurerm_network_interface" "vm01-nic" {
   name                = "vm01-nic"
   location            = azurerm_resource_group.rg.location
@@ -53,6 +61,7 @@ resource "azurerm_network_interface" "vm01-nic" {
     name                          = "vm01"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm01.id
   }
 }
 
