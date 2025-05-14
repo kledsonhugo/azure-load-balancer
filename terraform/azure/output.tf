2135d19842bae24248df26130f3d8905fecaa2d2
Kledson Basso <kledsonhugo@gmail.com>
2025-05-14 14:57:27 -0300

    Atualizar versão do Terraform para 1.11.4 e corrigir descrição do output do Load Balancer

--- terraform/azure/output.tf
+++ terraform/azure/output.tf
@@ -1,4 +1,19 @@
-output "lb_fqdn" {
+output "lb_url" {
   value       = "http://${azurerm_public_ip.lb.fqdn}"
-  description = "FQDN público do Load Balancer"
+  description = "URL pública do Load Balancer"
+}
+
+output "lb_public_ip" {
+  value       = azurerm_public_ip.lb.ip_address
+  description = "IP público do Load Balancer"
+}
+
+output "vm01_public_ip" {
+  value       = azurerm_public_ip.vm01.ip_address
+  description = "IP público da VM01"
+}
+
+output "vm02_public_ip" {
+  value       = azurerm_public_ip.vm02.ip_address
+  description = "IP público da VM02"
 }
