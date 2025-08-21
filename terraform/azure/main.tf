05e94bfce61984b9e3c2ea91f146c6620d0fd03a
Kledson Basso <kledsonhugo@gmail.com>
2025-08-21 08:26:29 -0300

    Atualizar rótulo de domínio do IP público do Load Balancer para "azureloadbalancer001"

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -171,7 +171,7 @@ resource "azurerm_public_ip" "lb" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  domain_name_label   = "staticsitelbtf0001"
+  domain_name_label   = "azureloadbalancer001"
   sku                 = "Standard"
 }
 
